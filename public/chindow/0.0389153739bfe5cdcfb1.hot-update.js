a8877e42c680294fef0c9d7cbaccad5556d33373
Dylan Harness <dharness@uwo.ca>
2017-03-19 14:07:41 -0400

    .

--- public/chindow/0.0389153739bfe5cdcfb1.hot-update.js
+++
@@ -1,1 +0,0 @@
-webpackHotUpdate(0,{6:function(n,e){throw new Error("Module build failed: Unknown word (20:20)\n\n  18 |   padding: 17px 20px;\n  19 |   border-radius: 6px;\n> 20 |   font-weight: 100;x\n     |                    ^\n  21 | }\n  22 | \n  23 | .sc-message--content.sent .sc-message--body {\n")}});
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new UglifyJSPlugin()
   ]
 };
