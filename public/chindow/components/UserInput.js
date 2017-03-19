01d5fb482142072ad3f7a8879f173aa681fa9d4f
Dylan Harness <dharness@uwo.ca>
2017-03-19 12:27:09 -0400

    fixed input box

--- public/chindow/components/UserInput.js
+++ public/chindow/components/UserInput.js
@@ -4,7 +4,7 @@ class UserInput extends Component {
 
   constructor() {
     super();
-    this.state = {inputValue: ''};
+    this.state = {inputValue: '', textareaRow: 1};
   }
 
   handleSubmit(e) {
@@ -14,20 +14,35 @@ class UserInput extends Component {
   }
 
   handleInputChange(e) {
-    console.log(e);
     this.setState({inputValue: e.target.value});
+  }
+
+  handleKey(event) {
+    if (event.keyCode == 13 && !event.shiftKey) {
+        event.preventDefault();
+        this.props.onSubmit(this.state.inputValue);
+        this.setState({inputValue: ''});
+    } else if (event.keyCode == 13) {
+      this.setState({textareaRow: ++this.state.textareaRow})
+    }
   }
 
   render () {
     return (
       <form className="sc-input-field" onSubmit={this.handleSubmit.bind(this)}>
-        <textarea
+        <div
+          contentEditable="true"
+          className="sc-input-field--input">
+          {this.state.inputValue}
+        </div>
+        {/*<textarea
           placeholder="Write a reply"
-          rows="1"
-          value={this.state.inputValue} 
-          onChange={this.handleInputChange.bind(this)} 
+          rows={this.state.textareaRow}
+          value={this.state.inputValue}
+          onKeyDown={this.handleKey.bind(this)}
+          onInput={this.handleInputChange.bind(this)} 
           className="sc-input-field--input" 
-          autocomplete="off"></textarea>
+          autocomplete="off"/>*/}
       </form>
     )
   }
