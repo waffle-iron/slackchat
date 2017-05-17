75ac8ede71153f9663a88a1bc56b03c6a5fb4c5c
Dylan Harness <dharness@uwo.ca>
2017-05-17 02:34:24 -0400

    added linting

--- services/slackAuth.js
+++ services/slackAuth.js
@@ -16,7 +16,7 @@ module.exports = {
           code,
           scope: ['channels:read','channels:write','channels:history','chat:write:user','chat:write:bot','team:read','users:read','bot','rtm:stream'],
           client_id: '94105311894.149174096865',
-          redirect_uri: 'http://chindow.io:9090',
+          redirect_uri: 'https://www.slackchat.io',
           client_secret: '059f7bea0e6e4558e0b1e5886986382e'          
         }));
   },
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,51 @@
 const path = require('path');
-const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 
-let ENV = {
-  'process.env': {
-    'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    'SC_SOCKET_URL': JSON.stringify('http://localhost:9090')
-  }
-}
-
-if (process.env.NODE_ENV === 'production') {
-  ENV['process.env'].SC_SOCKET_URL = JSON.stringify('http://138.197.149.23:9090')
-}
-
-
 module.exports = {
   devtool: 'source-map',
   entry: {
-    chindow: "./public/chindow",
-    dashboard: "./public/js/dashboard"
+    chindow: './public/chindow',
+    dashboard: './public/js/dashboard',
   },
   output: {
-    path: path.resolve(__dirname, "public/build"),
-    filename: "[name].bundle.js"
+    path: path.resolve(__dirname, 'public/build'),
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.js?$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
-          presets: ["es2015", "preact"]
-        }
+          presets: ['es2015', 'preact'],
+        },
       }, {
         test: /\.css?$/,
         exclude: /public\/chindow\/styles\/?\w*\.css/,
         use: ExtractTextPlugin.extract({
-          use: 'css-loader'
-        })
+          use: 'css-loader',
+        }),
       }, {
         test: /public\/chindow\/styles\/?\w*\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new UglifyJSPlugin({
-      sourceMap: true
+      sourceMap: true,
     }),
-    new webpack.DefinePlugin(ENV),
     new ExtractTextPlugin('styles.bundle.css'),
     new OptimizeCssAssetsPlugin({
       assetNameRegExp: /styles\.bundle\.css$/,
-      cssProcessorOptions: { discardComments: {removeAll: true } }
-    })
-  ]
+      cssProcessorOptions: {
+        discardComments: {
+          removeAll: true,
+        },
+      },
+    }),
+  ],
 };
