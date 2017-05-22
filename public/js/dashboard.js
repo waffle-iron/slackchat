fbfcb46146fe5575dae405e0d52d8cfe8d81c467
Dylan Harness <dharness@uwo.ca>
2017-05-22 15:10:12 -0400

    added copy

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,9 +1,19 @@
 import './../styles/index';
 import './jscolor.min';
 
-
+// Items in the dropdown are not clickable using hrefs
+// since the menu closes too quickly. Use data-route instead.
 document.querySelectorAll('a[data-route]').forEach((el) => {
   el.addEventListener('click', () => {
     window.location.href = el.getAttribute('data-route');
   });
 });
+
+const copyScriptButton = document.querySelector('#sc-copy-script-button');
+const copyScriptInput = document.querySelector('#sc-copy-script-input');
+if (copyScriptButton) {
+  copyScriptButton.addEventListener('click', () => {
+    copyScriptInput.select();
+    document.execCommand('copy');
+  });
+}
