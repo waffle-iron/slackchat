53c17e8d3405d0c025108e3e2c7120b22e3f975c
Dylan Harness <dharness@uwo.ca>
2017-06-06 19:51:19 -0400

    added emoji and send icon

--- public/chindow/components/UserInput.js
+++ public/chindow/components/UserInput.js
@@ -1,35 +1,45 @@
 import { h, Component } from 'preact';
+import SendIcon from './icons/SendIcon';
+import EmojiIcon from './icons/EmojiIcon';
+
 
 class UserInput extends Component {
 
   constructor() {
     super();
-    this.state = {inputValue: ''};
+    this.state = { inputValue: '', inputActive: false };
   }
 
   handleKey(event) {
     if (event.keyCode == 13 && !event.shiftKey) {
-        event.preventDefault();
-        let messageText = this.userInput.textContent;
-        this.props.onSubmit(messageText);
-        this.setState({inputValue: ''});
+      event.preventDefault();
+      const messageText = this.userInput.textContent;
+      this.props.onSubmit(messageText);
+      this.setState({ inputValue: '' });
     }
   }
 
-  render () {
+  render() {
     return (
-      <form className="sc-input-field">
+      <form className={"sc-input-field" + (this.state.inputActive ? " active" : "")}>
         <div
+          onFocus={() => { this.setState({ inputActive: true }); }}
+          onBlur={() => { this.setState({ inputActive: false }); }}
           ref={(e) => this.userInput = e}
           onKeyDown={this.handleKey.bind(this)}
           contentEditable="true"
           placeholder="Write a reply..."
-          className="sc-input-field--input">
+          className="sc-input-field--input"
+        >
           {this.state.inputValue}
         </div>
+        <div className="sc-input-field--buttons">
+          <EmojiIcon />
+          <SendIcon />
+        </div>
       </form>
-    )
+    );
   }
 }
 
-export default UserInput
+export default UserInput;
